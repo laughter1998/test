f3d9a8c12d26ae612abc7f6d2407c064f88d6fe1
laughter1998 <laughter98@gmail.com>
2020-06-11 13:16:18 +0900

    6월11일 플로팅 플레이스홀더

--- calculater/script.js
+++ calculater/script.js
@@ -9,9 +9,10 @@ function equal(){
     }
 }
 function clean(){
-    document.form.textview.value ="";
+    document.querySelector('.textview').value="";
 }
 function back(){
-    var exp = document.form.textview.value;
+    const exp = document.querySelector('.textview').value;
+    // var exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0, exp.length-1);
 }
